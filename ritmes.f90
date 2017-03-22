8234c5bcce2bfa91a9678c4f70ad3b7b77d15656
Oscar Fajardo Fontiveros <oscarcapote@hotmail.es>
2017-03-22 02:44:43 +0100

    Arreglat tot i afegit include amb declaracio de variables

--- ritmes.f90
+++ ritmes.f90
@@ -1,6 +1,6 @@
 module ritmes
 implicit none
-real(8) :: k1,k2,V3,V4,k5,k6,V7,V8,k9,k10,V11,V12!Ratios
+real(8) :: kCat1,kCat2,V3,V4,kCat5,kCat6,V7,V8,kCat9,kCat10,V11,V12!Ratios
 real(8) :: Km1,Km2,Km3,Km4,Km5,Km6,Km7,Km8,Km9,Km10,Km11,Km12,Km13,F,Kf
 
 contains
@@ -8,7 +8,7 @@ contains
 !Definim ses velocitats de reaccions numerades segons sa taula I
 function ni1(RasGTP,Raf,pRaf,ppERK) result(ni)
     real(8) :: ni,RasGTP,Raf,pRaf,ppERK
-    ni = k1*RasGTP*Raf/Km1
+    ni = kCat1*RasGTP*Raf/Km1
     ni = ni/(1.0d0+(Raf/Km1)+(pRaf/Km2))
     ni = ni*((1.0d0+(F*ppERK/Kf))/(1.0d0+(ppERK/Kf)))
     !print*,'-',F,ppERK,Kf,((1.0d0+(F*ppERK/Kf))/(1.0d0+(ppERK/Kf)))
@@ -16,7 +16,7 @@ end function
 
 function ni2(RasGTP,Raf,pRaf,ppERK) result(ni)
     real(8) :: ni,RasGTP,Raf,pRaf,ppERK
-    ni = k2*RasGTP*Raf/Km2
+    ni = kCat2*RasGTP*pRaf/Km2
     ni = ni/(1.0d0+(Raf/Km1)+(pRaf/Km2))
     ni = ni*((1.0d0+(F*ppERK/Kf))/(1.0d0+(ppERK/Kf)))
 end function
@@ -29,19 +29,19 @@ end function
 
 function ni4(ppRaf,pRaf) result(ni)
     real(8) :: ni,ppRaf,pRaf
-    ni = V4*ppRaf/Km4
+    ni = V4*pRaf/Km4
     ni = ni/(1.0d0+(ppRaf/Km3)+(pRaf/Km4))
 end function
 
 function ni5(ppRaf,MEK,pMEK) result(ni)
     real(8) :: ni,MEK,pMEK,ppRaf
-    ni = k5*ppRaf*MEK/Km5
+    ni = kCat5*ppRaf*MEK/Km5
     ni = ni/(1.0d0+(MEK/Km5)+(pMEK/Km6))
 end function
 
 function ni6(ppRaf,MEK,pMEK) result(ni)
     real(8) :: ni,MEK,pMEK,ppRaf
-    ni = k6*ppRaf*pMEK/Km6
+    ni = kCat6*ppRaf*pMEK/Km6
     ni = ni/(1.0d0+(MEK/Km5)+(pMEK/Km6))
 end function
 
@@ -59,13 +59,13 @@ end function
 
 function ni9(ppMEK,ERK,pERK) result(ni)
     real(8) :: ni,ppMEK,pERK,ERK
-    ni = k9*ppMEK*ERK/Km9
+    ni = kCat9*ppMEK*ERK/Km9
     ni = ni/(1.0d0+(ERK/Km9)+(pERK/Km10))
 end function
 
 function ni10(ppMEK,ERK,pERK) result(ni)
     real(8) :: ni,ppMEK,pERK,ERK
-    ni = k10*ppMEK*pERK/Km10
+    ni = kCat10*ppMEK*pERK/Km10
     ni = ni/(1.0d0+(ERK/Km9)+(pERK/Km10))
 end function
 
--- treball.f90
+++ treball.f90
@@ -3,78 +3,53 @@ use ritmes
 implicit none
 real(8),dimension(12) :: Pr!Ratios i probabilitats de donar una reaccio
 real(8) ::RasGTP,Raf,pRaf,ppRaf,MEK,pMEK,ppMEK,ERK,pERK,ppERK!Magnituds dinamiques
-real(8) :: suma,a0,factor
+real(8) :: suma,a0,factor,tFinal
 real(8) :: t,tau,rnd!Temps actual,temps estocastic i variable aleatoria entre 0 i 1
 integer(8) :: mu,MaxItt,itt
 
 !----------------------INICIALITZACIO---------------------
-!--Condicions inicials--
-    factor = 1.0d0/315.31118266529563d0
-    RasGTP=10.0d0
-    Raf=300.0d0
-    pRaf=0.0d0
-    ppRaf=0.0d0
-    MEK=300.0d0
-    pMEK=0.0d0
-    ppMEK=0.0d0
-    ERK=300.0d0
-    pERK=0.0d0
-    ppERK=0.0d0
-
-!--Parametres--
-    k1=1.0d0
-    k2=0.25d0
-    V3=100.0d0
-    V4=3.750d0
-    k5=2.5d0
-    k6=0.5d0
-    V7=3.0d0
-    V8=3.75d0
-    k9=0.125d0
-    k10=0.125d0
-    V11=3.75d0
-    V12=5.0d0
-
-
-    Km1=100.0d0
-    Km2=200.0d0
-    Km3=50.0d0
-    Km4=100.0d0
-    Km5=250.0d0
-    Km6=250.0d0
-    Km7=250.0d0
-    Km8=80.0d0
-    Km9=250.0d0
-    Km10=250.0d0
-    Km11=120.0d0
-    Km12=20.0d0
-    Km13=300.0d0
-    F=1.0d0!???
-    Kf=25.0d0!???
-
+!!--Condicions inicials--
+include 'declaration.dec'
 t=0
 itt=1
+tFinal = 1000
 MaxItt=500000!-1492324
 !call srand(9)
 !----------------------------------------------------------
 do
 !------------------UPDATE DE PROBABILITATS-----------------
 call prob_compute(Pr,a0)
+!print*,Raf,pRaf,ppRaf,MEK,pMEK,ppMEK,ERK,pERK,ppERK
+!print*,Pr!,a0,mu
+!read(*,*)
 !----------------------------------------------------------
 
 !-------------------MONTECARLO TIME STEP-------------------
-tau = -log(rand())/(a0)!Temps per que passi una reaccio
+tau = -log(rand())/(a0/factor)!Temps per que passi una reaccio
 rnd = rand()
 mu = 1!Reaccio qua pasara
 suma =Pr(mu)
+!print*,'Random',rnd
+!print*,'Probabilitats',Pr
 do while(rnd>suma)
     mu = mu+1
     suma = suma+Pr(mu)
 enddo
+!read(*,*)
+!do
+!    if((rnd>suma).and.(rnd<=suma+Pr(mu+1)))then
+!        mu = mu+1
+!        exit
+!    endif
+!    mu = mu+1
+!    suma = suma + Pr(mu)
+!enddo
+!print*,'Seleccionat',mu
+!read(*,*)
 t = t+(tau/60.0d0)
 call reaction(mu)
 print*,t,Raf,pRaf,ppRaf,MEK,pMEK,ppMEK,ERK,pERK,ppERK,Pr,a0,mu
-if(itt==MaxItt.or.t>100)then
+if(itt==MaxItt.or.t>tFinal)then
     stop
 endif
 itt=itt+1
@@ -100,7 +75,8 @@ subroutine prob_compute(Pr,a0)
     Pr(11) = ni11(ppERK,ERK,pERK)
     Pr(12) = ni12(ppERK,ERK,pERK)
     a0 = sum(Pr)
-    !print*,Pr,a0
+!    print*,Pr,a0
+!    read(*,*)
     !print*,Pr
     Pr = Pr/a0
 end subroutine
@@ -112,6 +88,7 @@ subroutine reaction(mu)
     if(mu==1)then
         Raf = Raf-factor
         pRaf = pRaf+factor
+        !print*,'AQUI!!!!',pRaf,Raf,RasGTP,ppERK,Pr(2)
     elseif(mu==2)then
         pRaf = pRaf-factor
         ppRaf = ppRaf+factor
@@ -136,6 +113,7 @@ subroutine reaction(mu)
     elseif(mu==9)then
         ERK = ERK - factor
         pERK = pERK + factor
+        !print*,'9:',ppMEK,pERK,factor,1.0d0/315.31118266529563d0
     elseif(mu==10)then
         pERK = pERK - factor
         ppERK = ppERK + factor
